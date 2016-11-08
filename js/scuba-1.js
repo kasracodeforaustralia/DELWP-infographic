845a532e4ced0f646498542aeba19c75e9f7b8ff
Diana Ramirez <diana@codeforaustralia.org>
2016-11-08 12:24:05 +1100

    Revert "Latest changes"
    This reverts commit 3d6db8302527779cc01238d41b89b639d0550c97.

--- js/scuba-1.js
+++ js/scuba-1.js
@@ -24,107 +24,107 @@ $(window).scroll(function(){
 
 
         /* scuba-1   */ 
-        if (topPerc >= 94.4){
-            $('#scuba-1').css({'display': 'none', 'left': '($winWidth  *(80) )/100', 'top':  '($winHeight *(96.5) )/100'});
-        } else  if ( topPerc < 94.2  && topPerc >= 94.4 ){
-            $('#scuba-1').css({'display': 'block', 'transform':'scale(0.5'});
+        if (topPerc >= 95.0){
+            $('#scuba-1').css({'display': 'none', 'transform':'scale(0.4)', 'left': '($winWidth  *(80) )/100', 'top':'($winHeight *(95.4) )/100'});
+        } else  if ( topPerc < 94.8  && topPerc >= 95.0 ){
+            $('#scuba-1').css({'display': 'block', 'transform':'scale(0.4)'});
             $('#scuba-1').animate({
                 width: ($WidthPerc * scuba1W)/100,
                 height:($WidthPerc * scuba1H)/100,
                 left: ($winWidth  *(80) )/100,
-                top:  ($winHeight *(96.5) )/100,
+                top:  ($winHeight *(95.4) )/100,
                 opacity: 0.4
  
             },0); 
             
-        }else if (  94.0 <= topPerc &&  topPerc < 94.2){
+        }else if (  94.6 <= topPerc &&  topPerc < 94.8){
            // $('#scuba-1').css({'transform':'scale(0.5) rotate(15deg)'});
             $('#scuba-1').animate({
        width: ($WidthPerc * scuba1W)/100,
                 height:($WidthPerc * scuba1H)/100,
-                left: ($winWidth  *(77) )/100,
-                top:  ($winHeight *(96.7) )/100,
-                opacity: 0.6,
+                left: ($winWidth  *(75.5) )/100,
+                top:  ($winHeight *(95.6) )/100,
+                opacity: 0.5,
                 start: function(){
-                    $( this ).css({'transform':'scale(0.6)'});
+                    $( this ).css({'transform':'scale(0.5) rotate(15deg)'});
                 }
             }, 0); 
             
-        }else if (  93.8 <= topPerc &&  topPerc < 94.0 ){
-             $('#scuba-1').css({'transform':'scale(0.6)'});
+        }else if (  94.4 <= topPerc &&  topPerc < 94.6 ){
+             $('#scuba-1').css({'transform':'scale(0.6) rotate(5deg)'});
              $('#scuba-1').animate({
       width: ($WidthPerc * scuba1W)/100,
                 height:($WidthPerc * scuba1H)/100,
                 left: ($winWidth  *(70) )/100,
-                top:  ($winHeight *(96.9) )/100,
+                top:  ($winHeight *(95.6) )/100,
                 opacity:0.7
             }, 0); 
             
-        } else if ( 93.6 <= topPerc &&  topPerc < 93.8 ){
+        } else if ( 94.2 <= topPerc &&  topPerc < 94.4 ){
               $('#scuba-1').css({'transform':'scale(0.8) rotate(-10deg)'});
              $('#scuba-1').animate({
      width: ($WidthPerc * scuba1W)/100,
                 height:($WidthPerc * scuba1H)/100,
                 left: ($winWidth  *(65.5) )/100,
-                top:  ($winHeight *(97.2) )/100,
+                top:  ($winHeight *(95.4) )/100,
                 opacity:0.8
             }, 0); 
            
-        }else if ( 93.4 <= topPerc &&  topPerc < 93.6 ){
+        }else if ( 94.0 <= topPerc &&  topPerc < 94.2 ){
             $('#scuba-1').css({'transform':'scale(0.9) rotate(5deg)'});
              $('#scuba-1').animate({
          width: ($WidthPerc * scuba1W)/100,
                 height:($WidthPerc * scuba1H)/100,
                 left: ($winWidth  *(60) )/100,
-                top:  ($winHeight *(97.0) )/100,
+                top:  ($winHeight *(95.2) )/100,
                 opacity: 0.9
             }, 0); 
-        }else if ( 93.2 <= topPerc &&  topPerc < 93.4 ){
+        }else if ( 93.8 <= topPerc &&  topPerc < 94.0 ){
             $('#scuba-1').css({'transform':'scale(1) rotate(2deg)'});
              $('#scuba-1').animate({
          width: ($WidthPerc * scuba1W)/100,
                 height:($WidthPerc * scuba1H)/100,
                 left: ($winWidth  *(55.5) )/100,
-                top:  ($winHeight *(97.0) )/100,
+                top:  ($winHeight *(95.2) )/100,
                 opacity: 1
             }, 0); 
-        }else if ( 93.0 <= topPerc &&  topPerc < 93.2 ){
+        }else if ( 93.6 <= topPerc &&  topPerc < 93.8 ){
             $('#scuba-1').css({'transform':'rotate(-5deg)'});
              $('#scuba-1').animate({
          width: ($WidthPerc * scuba1W)/100,
                 height:($WidthPerc * scuba1H)/100,
                 left: ($winWidth  *(52.5) )/100,
-                top:  ($winHeight *(97.0) )/100,
+                top:  ($winHeight *(97) )/100,
                 opacity: 1
             }, 0); 
-        }else if ( 92.8 <= topPerc &&  topPerc < 93.0 ){
+        }else if ( 93.4 <= topPerc &&  topPerc < 93.6 ){
             $('#scuba-1').css({'transform':'rotate(5deg)'});
              $('#scuba-1').animate({
          width: ($WidthPerc * scuba1W)/100,
                 height:($WidthPerc * scuba1H)/100,
                 left: ($winWidth  *(48.5) )/100,
-                top:  ($winHeight *(97.0) )/100,
+                top:  ($winHeight *(97) )/100,
                 opacity: 1
             }, 0); 
-        }/*else if ( 92.6 <= topPerc &&  topPerc < 92.8 ){
+        }else if ( 93.2 <= topPerc &&  topPerc < 93.4 ){
             $('#scuba-1').css({'transform':'rotate(-8deg)'});
              $('#scuba-1').animate({
          width: ($WidthPerc * scuba1W)/100,
                 height:($WidthPerc * scuba1H)/100,
                 left: ($winWidth  *(44.5) )/100,
-                top:  ($winHeight *(97.4) )/100,
+                top:  ($winHeight *(97) )/100,
                 opacity: 1
             }, 0);
-        }else if ( 92.4 <= topPerc &&  topPerc < 92.6 ){
+        }else if ( 93.0 <= topPerc &&  topPerc < 93.2 ){
             $('#scuba-1').css({'transform':'rotate(5deg)'});
              $('#scuba-1').animate({
          width: ($WidthPerc * scuba1W)/100,
                 height:($WidthPerc * scuba1H)/100,
                 left: ($winWidth  *(40.5) )/100,
-                top:  ($winHeight *(97.2) )/100,
+                top:  ($winHeight *(97) )/100,
                 opacity: 1
             }, 0);
-        }*/else if (topPerc < 94.4) {
+        }else if (topPerc < 95.0) {
             $('#scuba-1').css("display", 'block');
         }
 
